87c08717cd305da3688b11b9696efe1be8427bc8
junyoung <wns0818@naver.com>
2022-04-14 22:19:59 +0900

    '비밀정보'

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+app.use(express.json()) //For JSON requests
+app.use(express.urlencoded({extended: true}));
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://patric:1234@test.mtal7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입 할때 필요한 정보들을 client에서 가죠오ㅗ면 그것들을 데이터에서 가져옴
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json ({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
